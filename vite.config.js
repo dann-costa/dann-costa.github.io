fe79eee58d26a1cc3a649ad15b7e9ff3f97976a3
Daniel Costa <daniel2017gma@gmail.com>
2022-11-09 16:31:09 -0300

    initial profile

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ import tailwindConfig from './tailwind.config.mjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
-  // If you are deploying to https://<USERNAME>.github.io/<REPO>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
-  base: '/gitprofile/',
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwind(tailwindConfig), autoprefixer],
+    // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
+    // If you are deploying to https://<USERNAME>.github.io/<REPO>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
+    base: '/',
+    plugins: [react()],
+    css: {
+        postcss: {
+            plugins: [tailwind(tailwindConfig), autoprefixer],
+        },
     },
-  },
 });
